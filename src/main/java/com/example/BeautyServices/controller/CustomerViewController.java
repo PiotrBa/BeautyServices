7ac1c46772f4e9fae69137ee29852398574aeda9
PiotrBa <piotr.balazyk@wp.pl>
2023-10-21 13:12:29 +0100

    Code formatting

--- src/main/java/com/example/BeautyServices/controller/CustomerViewController.java
+++ src/main/java/com/example/BeautyServices/controller/CustomerViewController.java
@@ -1,7 +1,6 @@
 package com.example.BeautyServices.controller;
 
 import com.example.BeautyServices.entity.Customer;
-import com.example.BeautyServices.entity.Reservation;
 import com.example.BeautyServices.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,13 +13,13 @@ import java.util.Optional;
 @RequestMapping("/customers")
 @AllArgsConstructor
 public class CustomerViewController {
-    private final CustomerRepository repository;
+    private final CustomerRepository customerRepository;
     private final PasswordEncoder passwordEncoder;
 
 
     @GetMapping()
     public String getListView(Model model) {
-        model.addAttribute("customers", repository.findAll());
+        model.addAttribute("customers", customerRepository.findAll());
         return "/customer/customer-list";
     }
 
@@ -35,32 +34,32 @@ public class CustomerViewController {
         customer.setPassword(passwordEncoder.encode(customer.getPassword()));
         customer.setRole("ROLE_USER");
         customer.setActive(true);
-        repository.save(customer);
+        customerRepository.save(customer);
         return "redirect:/customers";
     }
 
     @GetMapping("/edit")
     public String editCustomerView(Model model, @RequestParam Long id){
-        model.addAttribute("customers", repository.findById(id));
+        model.addAttribute("customers", customerRepository.findById(id));
         return "/customer/customer-edit";
     }
 
     @PostMapping("edit")
     public String editCustomer(Customer customer, @RequestParam Long id) {
-        Optional<Customer> customerOptional = repository.findById(id);
+        Optional<Customer> customerOptional = customerRepository.findById(id);
         if (customerOptional.isPresent()) {
             Customer newCustomer = customerOptional.get();
             newCustomer.setName(customer.getName());
             newCustomer.setMobileNumber(customer.getMobileNumber());
             newCustomer.setEmail(customer.getEmail());
-            repository.save(newCustomer);
+            customerRepository.save(newCustomer);
         }
         return "redirect:/customers";
     }
 
     @GetMapping("/delete")
     public String deleteCustomerView(Model model, @RequestParam Long id){
-        Optional<Customer> customerOptional = repository.findById(id);
+        Optional<Customer> customerOptional = customerRepository.findById(id);
         if(customerOptional.isPresent()) {
             model.addAttribute("customers", customerOptional.get());
         }
@@ -69,10 +68,10 @@ public class CustomerViewController {
 
     @PostMapping("/delete")
     public String deleteCustomer(@RequestParam Long id){
-        Optional<Customer> customerOptional = repository.findById(id);
+        Optional<Customer> customerOptional = customerRepository.findById(id);
         if(customerOptional.isPresent()){
             Customer customer = customerOptional.get();
-            repository.delete(customer);
+            customerRepository.delete(customer);
         }
         return "redirect:/customers";
     }
