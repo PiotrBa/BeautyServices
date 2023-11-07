ec41ae52fc7fa4dfb0a62e1c039ce35a4455c2f0
PiotrBa <piotr.balazyk@wp.pl>
2023-11-07 18:41:48 +0000

    Logger for HomePageViewController

--- src/main/java/com/example/BeautyServices/controller/HomePageViewController.java
+++ src/main/java/com/example/BeautyServices/controller/HomePageViewController.java
@@ -6,6 +6,8 @@ import com.example.BeautyServices.repository.CustomerRepository;
 import com.example.BeautyServices.repository.ReservationRepository;
 import com.example.BeautyServices.repository.ServiceRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +28,11 @@ public class HomePageViewController {
     private final CustomerRepository customerRepository;
     private final ServiceRepository serviceRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(HomePageViewController.class);
+
     @GetMapping()
     public String showAppointmentsView(Model model, Principal principal){
+        logger.info("Request to fetch all reservations.");
         String userName = principal.getName();
         Customer customer = customerRepository.getByUsername(userName);
         List<Reservation> reservation = reservationRepository.findByCustomerName(customer.getName());
@@ -39,6 +44,7 @@ public class HomePageViewController {
 
     @GetMapping("/add")
     public String addAppointmentsView(Model model, Principal principal){
+        logger.info("Request to view page for adding a new reservation.");
         String userName = principal.getName();
         Customer customer = customerRepository.getByUsername(userName);
         model.addAttribute("reservations", new Reservation());
@@ -49,28 +55,34 @@ public class HomePageViewController {
 
     @PostMapping("/add")
     public String addAppointments(Reservation reservation, Principal principal) {
+        logger.info("Request to add a new reservation.");
         String userName = principal.getName();
         Customer customer = customerRepository.getByUsername(userName);
         LocalDateTime dateTimeNow = LocalDateTime.now();
         reservation.setCustomer(customer);
         reservation.setCreateReservation(dateTimeNow);
         reservationRepository.save(reservation);
+        logger.info("Reservation added successfully!!!");
         return "redirect:/homepage";
     }
 
     @GetMapping("/edit")
     public String editAppointmentsView(@RequestParam Long id, Model model) {
+        logger.info("Request to view page for editing reservation with id: {}", id);
         Optional<Reservation> reservationOptional = reservationRepository.findById(id);
         if (reservationOptional.isPresent()) {
             model.addAttribute("reservations", reservationOptional.get());
             model.addAttribute("customer", reservationOptional.get().getCustomer());
             model.addAttribute("serviceList", serviceRepository.findAll());
+        }else {
+            logger.info("Reservation with id: {} not found", id);
         }
         return "/homepage/homepage-edit";
     }
 
     @PostMapping("/edit")
     public String editAppointments(Reservation reservation, @RequestParam Long id) {
+        logger.info("Request to edit reservation with id: {}", id);
         Optional<Reservation> reservationOptional = reservationRepository.findById(id);
         if (reservationOptional.isPresent()) {
             LocalDateTime dateTimeNow = LocalDateTime.now();
@@ -79,26 +91,35 @@ public class HomePageViewController {
             newReservation.setAppointment(reservation.getAppointment());
             newReservation.setUpdateReservation(dateTimeNow);
             reservationRepository.save(newReservation);
+            logger.info("Reservation with id: {} updated successfully", id);
+        }else {
+            logger.error("Failed to update reservation with id: {} as it does not exist", id);
         }
         return "redirect:/homepage";
     }
 
     @GetMapping("/delete")
     public String deleteAppointmentsView(@RequestParam Long id, Model model) {
+        logger.info("Request to view page for deleting reservation with id: {}", id);
         Optional<Reservation> reservationOptional = reservationRepository.findById(id);
         if (reservationOptional.isPresent()){
             model.addAttribute("reservation", reservationOptional.get());
+        }else {
+            logger.error("Reservation with id: {} not found for deletion", id);
         }
-
         return "/homepage/homepage-delete";
     }
 
     @PostMapping("/delete")
     public String deleteAppointments(@RequestParam Long id) {
+        logger.info("Request to delete reservation with id: {}", id);
         Optional<Reservation> reservationOptional = reservationRepository.findById(id);
         if(reservationOptional.isPresent()){
             Reservation reservation = reservationOptional.get();
             reservationRepository.delete(reservation);
+            logger.info("Reservation with id: {} deleted successfully", id);
+        }else {
+            logger.error("Failed to delete reservation with id: {} as it does not exist", id);
         }
         return "redirect:/homepage";
     }
